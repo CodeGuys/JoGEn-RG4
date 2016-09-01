eff2b2b3f1bcb9c25b5cd24c2f119cfe3d68bef1
John Kayne <johnhoganuke2@gmail.com>
2016-09-01 09:07:10 -0400

    Update Var.java

--- JoGEn-rg4-r1/src/main/java/jogen/variable/Var.java
+++ JoGEn-rg4-r1/src/main/java/jogen/variable/Var.java
@@ -1,2 +1,21 @@
 package jogen.variable;
 
+public class Var<Class c> {
+  String name;
+  Object value;
+  
+  public Var<Class c>(String name) {
+    this.name = name;
+    value = null;
+  }
+  
+  public void set(Object value) {
+    this.value = value;
+  }
+  public void wipe() {
+    value = null;
+  }
+  public static void wipe(Var v) {
+    v.wipe();
+  }
+}
