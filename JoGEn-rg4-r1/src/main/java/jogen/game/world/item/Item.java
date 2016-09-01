b231a660d6caf898feb209f5030ea5c9e37b28f7
John Kayne <johnhoganuke2@gmail.com>
2016-09-01 09:12:51 -0400

    Update Item.java

--- JoGEn-rg4-r1/src/main/java/jogen/game/world/item/Item.java
+++ JoGEn-rg4-r1/src/main/java/jogen/game/world/item/Item.java
@@ -1,1 +1,14 @@
 package jogen.game.world.item;
+
+public class Item extends GameObject {
+  public Var name = new Var<String>("name");
+  public var id = new Var<ItemID>("name");
+  public var description = new Var<String>("description");
+  
+  public boolean compare(Item to) {
+    return (to.id.value.unique == this.id.value.unique);
+  }
+  public static boolean compare(Item a, Item b) {
+    return a.compare(b);
+  }
+}
