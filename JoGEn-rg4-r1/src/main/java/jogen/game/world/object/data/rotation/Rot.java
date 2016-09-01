16061a7c5b51c818da62ba16aeb12f989039b678
John Kayne <johnhoganuke2@gmail.com>
2016-09-01 09:21:58 -0400

    Create Rot.java

---
+++ JoGEn-rg4-r1/src/main/java/jogen/game/world/object/data/rotation/Rot.java
@@ -0,0 +1,7 @@
+pacakge jogen.game.world.object.data.rotation;
+
+public class Rot extends Loc {
+  public Var x = new Var<int>("x");
+  public Var y = new Var<int>("y");
+  public Var z = new Var<int>("z");
+}
