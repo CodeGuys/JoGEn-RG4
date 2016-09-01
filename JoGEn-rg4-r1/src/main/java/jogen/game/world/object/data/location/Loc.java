7ada83840e41cf5b9f22d2cd02c413512fd8d10d
John Kayne <johnhoganuke2@gmail.com>
2016-09-01 09:19:56 -0400

    Update Loc.java

--- JoGEn-rg4-r1/src/main/java/jogen/game/world/object/data/location/Loc.java
+++ JoGEn-rg4-r1/src/main/java/jogen/game/world/object/data/location/Loc.java
@@ -1,2 +1,18 @@
 package jogen.game.world.object.data.location;
 
+public class Loc {
+  public Var x = new Var<double>("x");
+  public Var y = new Var<double>("y");
+  public Var z = new Var<double>("z");
+  
+  public Loc() {
+    
+  }
+  
+  public static double getDist(Loc a, Loc b) {
+    return Math.sqrt(Math.sqr(b.x.value - a.x.value) + Math.sqr(b.y.value - a.y.value) + Math.sqr(b.z.value - a.z.value));
+  }
+  public double getDist(Loc b) {
+    return getDist(this, b);
+  }
+}
