db7d8853331c1ef5adc2e424c5f578e3795d2123
John Kayne <johnhoganuke2@gmail.com>
2016-09-01 09:03:06 -0400

    Update GameObject.java

--- JoGEn-rg4-r1/src/main/java/jogen/game/world/object/GameObject.java
+++ JoGEn-rg4-r1/src/main/java/jogen/game/world/object/GameObject.java
@@ -1,1 +1,14 @@
-public class GameObject {}
+package jogen.game.world.object;
+
+import jogen.variable.Var;
+import jogen.game.world.object.data.render.RenderClass;
+import jogen.game.world.object.data.location.Location;
+import jogen.game.world.object.data.rotation.Rotation;
+import jogen.game.world.object.data.composition.Composition;
+
+public class GameObject {
+  public Var location = new Var<Location>("location");
+  public Var rotation = new Var<Rotation>("rotation");
+  public Var compositoin = new Var<Composition>("composition");
+  public Var render = new Var<RenderClass>("render");
+}
