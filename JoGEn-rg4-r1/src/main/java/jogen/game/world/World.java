28f3d1f6d4ed17f34f179a1c703d9014fce64dd1
John Kayne <johnhoganuke2@gmail.com>
2016-09-02 14:54:45 -0400

    Update World.java

--- JoGEn-rg4-r1/src/main/java/jogen/game/world/World.java
+++ JoGEn-rg4-r1/src/main/java/jogen/game/world/World.java
@@ -1,2 +1,15 @@
 package jogen.game.world;
 
+public class World {
+  public String name;
+  public String path;
+  
+  public ArrayList<GameObject> objects = new ArrayList<GameObject>();
+  public ArrayList<Item> items = new ArrayList<Item>();
+  public ArrayList<Character> characters = new ArrayList<Character>();
+  
+  public World(String name, String path) {
+    this.name = name;
+    this.path = path;
+  }
+}
