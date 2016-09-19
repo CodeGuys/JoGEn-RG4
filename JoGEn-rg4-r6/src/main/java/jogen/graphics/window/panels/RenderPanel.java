b18c300977b5196c9ee46225941821fa9f5b7ad9
John Kayne <johnhoganuke2@gmail.com>
2016-09-19 08:32:49 -0400

    Update RenderPanel.java

--- JoGEn-rg4-r6/src/main/java/jogen/graphics/window/panels/RenderPanel.java
+++ JoGEn-rg4-r6/src/main/java/jogen/graphics/window/panels/RenderPanel.java
@@ -3,3 +3,38 @@ package jogen.graphics.window.panels;
 import javax.swing.*;
 import javax.media.j3d.*;
 
+public class RenderPanel extends JPanel implements Runnable {
+  private VirtualUniverse universe;
+  private Thread thread;
+  private boolean running;
+  
+  public RenderPanel(VirtualUniverse universe) {
+    this.universe = universe;
+    thread = new Thread(this);
+  }
+  
+  public void start() {
+    if (running) return;
+    thread.start();
+    running = true;
+  }
+  public void stop() {
+    if (!running) return;
+    try {
+      thread.join();
+    }
+    running = false;
+  }
+  
+  public void run() {
+    tick();
+    render();
+  }
+  
+  public void tick() {
+    
+  }
+  public void render() {
+    
+  }
+}
