6400a1a7bbc57cd2904b2ec93ec22de6fea25d64
John Kayne <johnhoganuke2@gmail.com>
2016-09-19 08:15:42 -0400

    Update DisplayUniverse.java

--- JoGEn-rg4-r6/src/main/java/jogen/graphics/universe/DisplayUniverse.java
+++ JoGEn-rg4-r6/src/main/java/jogen/graphics/universe/DisplayUniverse.java
@@ -2,4 +2,8 @@ package jogen.graphics.universe;
 
 import javax.media.j3d.VirtualUniverse;
 
-public class DisplayUniverse() extends VirtualUniverse 
+public class DisplayUniverse extends VirtualUniverse {
+  // Java3D is a complete bitch to code with. I'll do this later.
+  
+  public DisplayUniverse() {}
+}
